a42b9596fed56489e5d206bcc8df5fee6b1d549a
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-12 19:33:03 +0100

    Learning Python

---
+++ Day_1/First_Project.py
@@ -0,0 +1,7 @@
+print("El nombre ideal de tu cerveza es : " + input("¿Cual es tu animal Favorito?\n") + input("¿Las 3 inciales de tu nombre?\n"))
+
+
+FirstQuestion = input("¿Cuál es tu animal favorito?: \n")
+SecondQuestion = input("¿Escribe las 3 primeras iniciales de tu nombre?: \n")
+
+print("El nombre ideal para tu cerveza es: \"" + FirstQuestion + SecondQuestion + "\"")
---
+++ Day_2/Variables.py
@@ -0,0 +1,2 @@
+name1= input("Write your name \n");
+print("Your name is :" + " " + "\"" + name1 + "\"")
