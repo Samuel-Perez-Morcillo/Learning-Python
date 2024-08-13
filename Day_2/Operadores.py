2d1f033483c122e805d0fec881365fb0bccb4eba
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-13 12:57:41 +0100

    operators

---
+++ Day_2/Operadores.py
@@ -0,0 +1,15 @@
+x = 6
+y = 2
+z = 7
+
+print(f"{x} más {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} entre {y} es igual a {x/y}")
+
+
+print(f"{z} entre {y} redondeada hacia abajo es {z//y} y sin redondear era {z/y}")
+print(f"{z} módulo de {y} es igual a {z%y}")
+print(f"{x} elevado a la {y} es igual a {x**y}")
+print(f"La raiz cuadrada de {x} es igual a {x**0.5}")
+
