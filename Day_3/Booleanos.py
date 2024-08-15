55bc4653334c1d3c1883ac1480cc98446414d637
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-15 20:16:54 +0100

    booleans

---
+++ Day_3/Booleanos.py
@@ -0,0 +1,15 @@
+# Booleans solo pueden tener dos valores (True) or (False)
+
+numero = 5 > 2+6
+print(numero)
+
+# Como hemos visto en el ejemplo anterior los booleans nos permiten hacer comparaciones
+
+comparacion1 = 5 != 5   # Con el simbolo de exclamacion delante del igual pregunto si es distinto
+print(comparacion1)
+
+# Podremos revisar si ciertos valores se encuentran en las listas
+lista = [1, 2, 3, 4, "hola"]
+control = 5 in lista    # Como no esta me deberia devolver el valor False
+print(control)
+
