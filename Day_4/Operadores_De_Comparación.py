1163acdf661a8cb1ac1959b3d8ad5345b04bb3e3
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-20 11:35:24 +0100

    operadores de comparación

---
+++ Day_4/Operadores_De_Comparación.py
@@ -0,0 +1,18 @@
+# Para usar el Igual como asignación de variables sera así:
+variable = "hola mundo"
+print(variable)
+
+# Para usar el igual como comparador sera así:
+mi_bool = "blanco" == "negro"
+print(mi_bool)
+
+# Para comparar diferencia usaremos
+mi_bool = 100 != 100
+print(mi_bool)
+
+# Los camparadores de mayor y menor
+mi_bool = 100 > 100
+print(mi_bool)
+
+mi_bool2 = 100 > 100
+print(mi_bool)
