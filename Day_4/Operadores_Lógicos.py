c9c0b75d3847b04fad18670880f283186310276c
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-20 11:56:46 +0100

    Logical Operators

---
+++ Day_4/Operadores_Lógicos.py
@@ -0,0 +1,18 @@
+# Los operadores Lógicos son tres AND, OR, NOT
+
+#Ejemplos
+mi_bool = (6 < 5) and (7 < 9)
+print(mi_bool)
+
+
+mi_bool = (6 < 5) or (7 < 9)
+print(mi_bool)
+
+texto = "Este es un texto breve de prueba"
+palabra = ("breve" in texto) or ("python" in texto)
+print(palabra)
+
+
+mi_bool = not ("a" == "a")
+print(mi_bool)
+
