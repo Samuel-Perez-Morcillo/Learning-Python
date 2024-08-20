a8d0287d39563c405e28c159e8cb2ddb0fe4533d
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-20 20:20:37 +0100

    Range

--- Day_4/Loop While.py
+++ Day_4/Loop While.py
@@ -1,20 +1,37 @@
 # Es importante saber que los bucles while deben tener una condición que haga que
 # el bucle no se repita infinitas veces
 
+print("\n")
 monedas = 5
-
 while monedas > 0:
     print(f"Tengo {monedas} monedas")
     monedas -= 1
 else:
     print("No tengo mas dinero")
 
-
+print("\n")
 respuesta = "s"
-
 while respuesta == "s":
     respuesta = input("Quieres seguir? (s/n): ")
 else:
     print("gracias")
 
+print("\n")
+numero = 50
+while numero >= 0:
+    if numero % 5 == 0:
+        print(numero)
+        numero -= 1
+    else:
+        numero -= 1
+
+print("\n")
 # Break//Continue//Pass// son importantes en los bucles
+
+lista_numeros = [4, 5, 8, 7, 6, 9, 8, 2, 4, 5, 7, 1, 9, 5, 6, -1, -5, 6, -6, -4, -3]
+
+for each in lista_numeros:
+    if each < 0:
+        break
+    else:
+        print(each)
---
+++ Day_4/Rango.py
@@ -0,0 +1,15 @@
+print("\n")
+for numero in range(1, 5):  # El número hasta el que va no es inclusivo
+    print(numero)
+
+print("\n")
+lista = list(range(1, 101))
+print(lista)
+
+print("\n")
+suma_cuadrados = 0
+
+for each in range(1, 16):
+    cuadrado = each ** 2
+    suma_cuadrados += cuadrado
+
