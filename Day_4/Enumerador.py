2b8244c340aa9e9d8cc112bb66676f445cdbb45e
Samuel Pérez <samuelperez82pm@gmail.com>
2024-08-27 09:40:18 +0100

    Examples of Enumerate

--- Day_4/Enumerador.py
+++ Day_4/Enumerador.py
@@ -12,3 +12,25 @@ lista = ["a", "b", "c"]
 
 for item in enumerate(lista):
     print(indice, item)
+
+# Ejemplo 1
+
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+
+for indice , nombre in enumerate(lista_nombres):
+    print(f'{nombre} se encuentra en el índice {indice}')
+
+# Ejemplo 2
+
+string = "Python"
+
+lista_indices = [(indice, caracter) for indice, caracter in enumerate(string)]
+
+print(lista_indices)
+
+# Ejemplo 3
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano","Melisa"]
+
+for indice, nombre in enumerate(lista_nombres):
+    if nombre.startswith("M"):
+        print(indice)
