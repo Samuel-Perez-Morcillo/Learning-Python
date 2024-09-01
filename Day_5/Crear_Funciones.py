e29ba621d8469ddbfa571e108c53f67e6d6786c8
Samuel Pérez <samuelperez82pm@gmail.com>
2024-09-01 13:15:04 +0100

    Create_Functions

---
+++ Day_5/Crear_Funciones.py
@@ -0,0 +1,23 @@
+def saludar_persona(nombre):
+    """Esta funcion servirá para saludar a las personas"""
+    print("Hola " + nombre)
+
+
+saludar_persona("Fernando")
+
+# Ejemplo 2
+def bienvenida(nombre_persona):
+    print(f"¡Bienvenido {nombre_persona}!")
+
+nombre_persona = "Samu"
+
+bienvenida(nombre_persona)
+
+#Ejemplo 3
+def cuadrado(un_numero):
+    print(un_numero ** 2)
+
+un_numero = 2
+
+cuadrado(un_numero)
+
