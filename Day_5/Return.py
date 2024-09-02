3dab5f5c16aa7fba63bb173471fd603cf148ec9b
Samuel Pérez <samuelperez82pm@gmail.com>
2024-09-02 22:03:04 +0100

    fixed return learning

--- Day_5/Return.py
+++ Day_5/Return.py
@@ -1,22 +1,25 @@
+# Primero se define
 def multiplicar(numero1, numero2):
     return numero1 * numero2
 
-
+# Luego se invoca
 resultado = multiplicar(5, 10)
 print(resultado)
+
 
 # Ejemplo 1
 def usd_a_eur(dolares):
     return dolares * 0.90
 
+
 dolares = 5
 
 print(usd_a_eur(dolares))
 
+
 # Ejemplo 2
 def invertir_palabra(palabra):
     return palabra[::-1].upper()
-
 
 palabra = "Marta"
 print(invertir_palabra(palabra))
