ec5520afc91853a8131511ac08548b784123ef35
Samuel Pérez <samuelperez82pm@gmail.com>
2024-09-03 20:51:42 +0100

    dynamic functions and exercises

--- Day_5/Funciones_Dinamicas.py
+++ Day_5/Funciones_Dinamicas.py
@@ -8,7 +8,6 @@ print(resultado)
 
 
 def chequear_3_cifras(lista):
-
     lista_3_cifras = []
 
     for n in lista:
@@ -22,3 +21,42 @@ def chequear_3_cifras(lista):
 
 resultado = chequear_3_cifras([23, 440, 2000])
 print(resultado)
+
+
+# Ejemplo 1
+def todos_positivos(lista_numeros):
+    for n in lista_numeros:
+        if n < 0:
+            return False
+
+    return True
+
+
+lista_numeros = [22, -55, 33, 12]
+
+
+# Ejemplo 2
+def suma_menores(lista_numeros):
+    suma = 0
+
+    for n in lista_numeros:
+        if n in range(0, 1000):
+            suma += n
+
+    return suma
+
+
+lista_numeros = [100, 500, -10, 1500, 999, 0, 450]
+
+
+# Ejemplo 3
+def cantidad_pares(lista_numeros):
+    cuenta = 0
+
+    for n in lista_numeros:
+        if n % 2 == 0:
+            cuenta += 1
+    return cuenta
+
+
+lista_numeros = [1, 2, 4, 5, 6, 7, 8, 9, 10]
