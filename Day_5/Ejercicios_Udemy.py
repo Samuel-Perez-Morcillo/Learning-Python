220cc553551661af7ea7189aa21ce64fe310b79c
Samuel Pérez <samuelperez82pm@gmail.com>
2024-09-09 20:46:08 +0100

    Exercises of Udemy

--- Day_5/Ejercicios_Udemy.py
+++ Day_5/Ejercicios_Udemy.py
@@ -1,4 +1,4 @@
-def devolver_distintos(num1, num2, num3):
+"""def devolver_distintos(num1, num2, num3):
     suma = num1 + num2 + num3
 
     if suma > 15:
@@ -7,9 +7,9 @@ def devolver_distintos(num1, num2, num3):
     elif suma < 10:
         print("Al resultar la suma menor a 10 se muestra el numero minimo que has ingresado: ")
         return print(min(num1, num2, num3))
-    elif 10 >= suma >= 15:
+    else:
         print("Al resultar la suma entre 10 y 15 incluidos se muestra el numero intermedio que has ingresado: ")
-        return sorted(num1, num2, num3)
+        return print(sorted([num1, num2, num3])[1])
 
 
 num1 = int(input("Ingresa el primer numero a sumar: "))
@@ -17,4 +17,66 @@ num2 = int(input("Ingresa el segundo numero a sumar: "))
 num3 = int(input("Ingresa el tercer numero a sumar: "))
 
 devolver_distintos(num1, num2, num2)
+"""
 
+
+# Ejercicio 2
+
+def palabra_del_reves(palabra):
+    letras_unicas = set(palabra.lower())
+    resultado = sorted(letras_unicas)
+    return resultado
+
+
+prueba = "Monja"
+print(palabra_del_reves(prueba))
+
+
+# Ejercicio 3
+def doble_cero(*args):
+    for n in range(len(args) - 1):
+        if args[n] == 0 and args[n + 1] == 0:
+            return True
+    return False
+
+
+print(doble_cero(1, 2, 34, 5, 5, 6, 7, 1, 0, 0))
+
+
+# Ejercicio 4
+
+def contar_pares_impares(num):
+    contador_pares = 0
+    contador_impares = 0
+    for n in range(0, num + 1):
+        if n % 2 == 0 and n != 0:
+            contador_pares += 1
+        elif n % 2 != 0 and n != 1:
+            contador_impares += 1
+    return print(f"Hasta el numero {num}, Hay {contador_pares} primos y {contador_impares} impares")
+
+
+print(contar_pares_impares(6))
+
+
+# Ejercicio 5
+def es_primo(num):
+    if num < 2:
+        return False
+    for n in range(2, int(num ** 0.5) + 1):
+        if num % n == 0:
+            return False
+    return True
+
+
+def contar_primos(limite):
+    contador = 0
+    for num in range(limite + 1):
+        if es_primo(num):
+            print(num)
+            contador += 1
+    print(f"Cantidad de numeros primos encontrados: {contador}")
+    return contador
+
+
+print(contar_primos(8))
