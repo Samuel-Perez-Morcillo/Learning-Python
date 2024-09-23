22284ba403f21916d289fb43d569a6788ddb9de1
Samuel Pérez <samuelperez82pm@gmail.com>
2024-09-23 20:06:39 +0100

    update

--- Day_6/Create_and_Write_Documents.py
+++ Day_6/Create_and_Write_Documents.py
@@ -4,3 +4,10 @@ archivo.write("Actualizado 19:56")
 
 
 archivo.close()
+
+"""
+Hay que tener en cuenta que el metodo de escritura al poner W, sobreescribirá toda la información
+que contenia el documento.
+Al usar "a" no se sobreescribirá la información, simplemente se escribirá lo que queramos al final del documento
+Al usar "r" solo se abrirá el documento y no se podrá escribir.
+"""
